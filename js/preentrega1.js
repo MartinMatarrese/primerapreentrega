08ed8b8a8af631b5b2071dd6775a778953d9be5b
Martin Matarrese <matarresemartin@gmail.com>
2024-05-20 10:38:43 -0300

    Primera version

---
+++ js/preentrega1.js
@@ -0,0 +1,27 @@
+function saludo(){
+    alert("Bienvenidos a nuestra pagina");
+}
+saludo();
+
+let ingresarEdad;
+
+do{
+    ingresarEdad = parseInt(prompt("Ingrese su edad"));    
+    if(ingresarEdad >= 18){
+        alert("Su edad es de " + ingresarEdad + " años, por lo tanto es Mayor de Edad");
+    } 
+    else if(ingresarEdad > 0 && ingresarEdad < 18){
+        alert("Su Edad es de " + ingresarEdad + " años, por lo tanto es Menor de Edad, debe ser Mayor de Edadd");
+    }    
+    else if(ingresarEdad <= 0){
+        alert("La Edad ingresada debe ser mayor a 0");
+    }
+    else{
+        alert("Debe ingresar un valor númerico !!!"); 
+    }
+}
+while(ingresarEdad <= 0 || ingresarEdad < 18 || !ingresarEdad);
+
+function despedida(){
+    alert("Gracias por visitar nuestra pagina");
+}
